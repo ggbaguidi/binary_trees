1468a3516ddd311e11c1cb6a03ba2b6f9dbd631c
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-08-28 07:32:54 -0700

    [UPDATE, MODIFY, ADD] update .gitignore, binary_trees.h files, modify 4-binary_tree_is_leaf.c and add a new file named 5-binary_tree_is_root.c

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,7 @@ void binary_tree_delete(binary_tree_t *tree);
 
 /*checks if a node is a leaf*/
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/*check if a given node is a root*/
+int binary_tree_is_root(const binary_tree_t *node);
 #endif
