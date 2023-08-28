4f290aae53ca6950dd4daf4336a33999b98a5cfe
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-08-28 07:10:41 -0700

    [UPDATE, ADD] update the header file and add a new file named 3-binary_tree_delete.c

--- binary_trees.h
+++ binary_trees.h
@@ -44,4 +44,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /*insert a node as the right-child of another node*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/*delete an entire binary tree*/
+void binary_tree_delete(binary_tree_t *tree);
 #endif
