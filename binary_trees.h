58fa85dc6082352fe035e1e1f7faaf8fdba2e599
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-08-30 11:09:17 -0700

    [ADD] add two other files

--- binary_trees.h
+++ binary_trees.h
@@ -59,4 +59,11 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 /*binary tree using in-order traversal*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/*binary tree using post-order traversal*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/*measure the height of a binary tree*/
+size_t __binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
 #endif
