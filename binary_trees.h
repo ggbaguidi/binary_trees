b736b99f27f4f5c5954436e4af896b125e1da0fc
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-08-31 05:12:41 -0700

    [UPDATE, ADD] update the header file and add a new file to measure the balance factor for binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -82,4 +82,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 /*counts the nodes with at least 1 child in a binary tree*/
 size_t __binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/*measure the balance factor of a binary tree*/
+int binary_tree_balance(const binary_tree_t *tree);
 #endif
